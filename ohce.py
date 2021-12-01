a5174265d86d9d0e47bdb05f42e2b228f5bae0df
Daniil Rosso <daniil.rosso@efrei.net>
2021-12-01 15:25:44 +0100

    Initial commit v2 tkt

---
+++ ohce.py
@@ -0,0 +1,11 @@
+#> cd c:\path\to\project
+#> python -m venv .venv
+#> .venv\Scripts\activate.bat
+#> python -m pip install -U pip
+#> pip install pytest
+
+
+def hi():
+        print("Hello :)")
+
+hi()
