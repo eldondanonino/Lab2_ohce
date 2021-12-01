84b365b3651d7e8ab3545142b3009023eeddcb09
Daniil Rosso <daniil.rosso@efrei.net>
2021-12-01 16:13:08 +0100

    Added Console Class
    trying out classes

---
+++ classes/Console.py
@@ -0,0 +1,8 @@
+from methods import greet
+
+class Console:
+    name = "Bob"
+    def changeName(self):
+        self.name = input()
+
+    
--- ohce.py
+++ ohce.py
@@ -1,14 +1,11 @@
+from classes.Console import Console
 from methods import greet
-
+from classes.Console import Console
 
 def hi():
-        print("Hello :)")
-
-        # TODO
-        greet()
-        #reverseEcho()
-        #likeInput()
-
-
+    bob = Console
+    print("Hi, what's your name?")
+    bob.changeName(bob)
+    print(f"Buenas dias {bob.name}")
 
 hi()
